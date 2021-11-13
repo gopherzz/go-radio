dff2b0730e2c6922d08bfdbe50c799c7a8ce9622
gopherzz <kazekin99@gmail.com>
2021-11-13 21:31:29 +0000

    added NOT scheme

---
+++ pkg/radio/elements/not.go
@@ -0,0 +1,27 @@
+package elements
+
+import (
+	"go-radio/pkg/radio/signal"
+)
+
+// And Logical Operation scheme
+type not struct {
+}
+
+func Not() *not {
+	return &and{}
+}
+
+func (n not) Eval(signals []signal.Signal) []signal.Signal {
+	return []signal.Signal{
+		signal.New(!signals[0].Turn),
+	}
+}
+
+func (n not) InputsLength() int {
+	return 1
+}
+
+func (n not) OutputLength() int {
+	return 1
+}
